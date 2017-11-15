05bbe5e4ab22f08503c5bbcb01a1f4ed9d668a37
edwincarbajal <exaviercarbajal@gmail.com>
2017-11-15 17:20:20 -0500

    Add sticky feature to navigation and secondary contents

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Navigation from '../Navigation'
 import PrimaryContent from '../PrimaryContent'
 import SecondaryContent from '../SecondaryContent'
@@ -7,22 +7,38 @@ import {
   Grid
 } from 'semantic-ui-react'
 
-const Main = () => (
-  <Container>
-    <Grid padded="vertically">
-      <Grid.Row>
-        <Grid.Column width={3}>
-          <Navigation />
-        </Grid.Column>
-        <Grid.Column width={8}>
-          <PrimaryContent />
-        </Grid.Column>
-        <Grid.Column width={5}>
-          <SecondaryContent />
-        </Grid.Column>
-      </Grid.Row>
-    </Grid>
-  </Container>
-)
+class Main extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {}
+  }
+
+  handleContextRef = contextRef => this.setState({ contextRef })
+
+  render() {
+    const { contextRef } = this.state
+
+    return(
+      <Container>
+        <div ref={this.handleContextRef}>
+          <Grid padded="vertically">
+            <Grid.Row>
+              <Grid.Column width={3}>
+                <Navigation contextRef={contextRef} />
+              </Grid.Column>
+              <Grid.Column width={8}>
+                <PrimaryContent />
+              </Grid.Column>
+              <Grid.Column width={5}>
+                <SecondaryContent contextRef={contextRef} />
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </div>
+      </Container>
+    )
+  }
+}
 
 export default Main
