b46f522c4f806c07f9f2e3ab3357bce2e18ad697
edwincarbajal <exaviercarbajal@gmail.com>
2017-12-06 16:02:04 -0500

    Create a seperate reducer for individual post

--- src/reducers/post.js
+++ src/reducers/post.js
@@ -1,23 +1,26 @@
-import { REQUEST_POSTS, RECEIVE_POSTS } from '../constants/actionTypes';
+import {
+  REQUEST_INDIVIDUAL_POST,
+  RECEIVE_INDIVIDUAL_POST
+} from '../constants/actionTypes';
 
 const initialState = {
   isFetching: false,
-  items: []
+  item: []
 };
 
 export default (state = initialState, action) => {
   switch (action.type) {
-    case REQUEST_POSTS:
+    case REQUEST_INDIVIDUAL_POST:
       return {
         ...state,
         isFetching: !state.isFetching
       };
-    case RECEIVE_POSTS:
+    case RECEIVE_INDIVIDUAL_POST:
       return {
         ...state,
         isFetching: !state.isFetching,
         lastUpdated: action.receivedAt,
-        items: action.posts
+        item: action.post
       };
     default:
       return state;
