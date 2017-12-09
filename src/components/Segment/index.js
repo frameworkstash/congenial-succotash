26ee775437a42a217b37a7704abf57209a5adb7b
Edwin Carbajal <exaviercarbajal@gmail.com>
2017-12-09 18:16:46 -0500

    Update index.js

--- src/components/Segment/index.js
+++ src/components/Segment/index.js
@@ -18,7 +18,7 @@ const Welcome = props => (
             latest Web, Mobile, AI, IoT, VR and many more articles.
           </p>
           <Button
-            color="teal"
+            color="green"
             size="large"
             onClick={() => {
               props.openModal('MODAL_TYPE_LOGIN');
