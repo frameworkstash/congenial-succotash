4c9ed984725b805b99cfd9cde504d08f3638eb0b
edwincarbajal <exaviercarbajal@gmail.com>
2018-05-24 18:41:21 -0400

    Remove social login and add email + password login

--- src/containers/Modals/LogIn.js
+++ src/containers/Modals/LogIn.js
@@ -1,54 +1,83 @@
+import agent from '../../agent';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
 import ModalRoot from '../../components/ModalRoot';
-import {
-  signInWithGithub,
-  signInWithFacebook,
-  signInWithTwitter
-} from '../../actions/authActions';
-import { Button, Grid, Header, Icon, Modal } from 'semantic-ui-react';
+import { LOGIN } from '../../constants/actionTypes';
+import { Button, Form, Grid, Header, Modal } from 'semantic-ui-react';
 
 class LogIn extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: ''
+    };
+  }
+
+  componentWillUnMount() {
+    this.setState({
+      email: '',
+      password: ''
+    });
+  }
+
+  handleChange = (e, { name, value }) => {
+    this.setState({ [name]: value });
+  };
+
+  submitForm = (email, password) => ev => {
+    ev.preventDefault();
+    this.props.handleSubmit(email, password);
+  };
+
   render() {
+    const { email, password } = this.state;
     return (
       <ModalRoot>
         <Modal.Content>
-          <Grid textAlign="center" columns="equal" padded stretched>
-            <Grid.Column>
-              <Header as="h2">Login to Join The Community</Header>
-              <p>
-                Frameworkstash is a community to share and geek out about<br />the
-                latest tutorials. Join us :)
-              </p>
-            </Grid.Column>
-
+          <Grid columns={2} textAlign="center" padded stretched centered>
             <Grid.Row>
-              <Grid.Column>
-                <Button onClick={() => signInWithTwitter()} color="twitter">
-                  <Icon name="twitter" /> LOGIN WITH TWITTER
-                </Button>
-              </Grid.Column>
-
-              <Grid.Column>
-                <Button color="facebook">
-                  <Icon name="facebook" /> LOGIN WITH FACEBOOK
-                </Button>
-              </Grid.Column>
-
-              <Grid.Column>
-                <Button onClick={() => signInWithGithub()} color="black">
-                  <Icon name="github" /> LOGIN WITH GITHUB
-                </Button>
+              <Grid.Column textAlign="center">
+                <Header as="h2">Login to Join The Community</Header>
+                <p>
+                  Frameworkstash is a community to share and geek out about<br />the
+                  latest tutorials. Join us :)
+                </p>
               </Grid.Column>
             </Grid.Row>
 
             <Grid.Column>
-              <p>
-                We'll never post to any of your accounts without your
-                permission.
-              </p>
+              <Form onSubmit={this.submitForm(email, password)} width="equal">
+                <Form.Input
+                  fluid
+                  icon="user"
+                  iconPosition="left"
+                  placeholder="Email"
+                  name="email"
+                  value={email}
+                  onChange={this.handleChange}
+                />
+                <Form.Input
+                  fluid
+                  icon="lock"
+                  iconPosition="left"
+                  placeholder="Password"
+                  type="password"
+                  name="password"
+                  value={password}
+                  onChange={this.handleChange}
+                />
+                <Button type="submit" color="blue" fluid>
+                  Login
+                </Button>
+              </Form>
             </Grid.Column>
+
+            <Grid.Row>
+              <Grid.Column textAlign="center">
+                <p>Don't have an account? Sign up with us.</p>
+              </Grid.Column>
+            </Grid.Row>
           </Grid>
         </Modal.Content>
       </ModalRoot>
@@ -56,14 +85,9 @@ class LogIn extends Component {
   }
 }
 
-const mapDispatchToProps = dispatch =>
-  bindActionCreators(
-    {
-      signInWithGithub,
-      signInWithFacebook,
-      signInWithTwitter
-    },
-    dispatch
-  );
+const mapDispatchToProps = dispatch => ({
+  handleSubmit: (email, password) =>
+    dispatch({ type: LOGIN, payload: agent.Auth.login(email, password) })
+});
 
 export default connect(null, mapDispatchToProps)(LogIn);
