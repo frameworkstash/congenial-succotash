89c0af2002c8dee569d7a6e3c00a965adcc3889c
edwincarbajal <exaviercarbajal@gmail.com>
2018-05-24 18:32:23 -0400

    Refactor and seperate API logic

---
+++ src/agent.js
@@ -0,0 +1,73 @@
+import superagentPromise from 'superagent-promise';
+import _superagent from 'superagent';
+
+const superagent = superagentPromise(_superagent, global.Promise);
+
+const API_ROOT = '/api';
+
+const encode = encodeURIComponent;
+const responseBody = res => res.body.data;
+
+let token = null;
+const tokenPlugin = req => {
+  if (token) {
+    req.set('Authorization', `Bearer ${token}`);
+  }
+};
+
+const requests = {
+  del: url =>
+    superagent
+      .del(`${API_ROOT}${url}`)
+      .use(tokenPlugin)
+      .then(responseBody),
+  get: url =>
+    superagent
+      .get(`${API_ROOT}${url}`)
+      .use(tokenPlugin)
+      .then(responseBody),
+  put: (url, body) =>
+    superagent
+      .put(`${API_ROOT}${url}`, body)
+      .use(tokenPlugin)
+      .then(responseBody),
+  post: (url, body) =>
+    superagent
+      .post(`${API_ROOT}${url}`, body)
+      .use(tokenPlugin)
+      .then(responseBody)
+};
+
+const Auth = {
+  current: () => requests.get('/user'),
+  login: (email, password) =>
+    requests.post('/auth/login', {
+      auth: { email: email, password: password }
+    }),
+  register: (username, email, password) =>
+    requests.post('/users', { user: { username, email, password } }),
+  save: user => requests.put('/user', { user })
+};
+
+const Articles = {
+  favoritedBy: username =>
+    requests.get(`/tutorials?upvoted=${encode(username)}`),
+  submittedBy: username =>
+    requests.get(`/tutorials?submitted=${encode(username)}`),
+  madeBy: username => requests.get(`/tutorials?made=${encode(username)}`)
+};
+
+const Profile = {
+  follow: username => requests.post(`/profiles/${username}/follow`),
+  get: username => requests.get(`/profiles/${username}`),
+  unfollow: username => requests.del(`/profiles/${username}/follow`)
+};
+
+export default {
+  Auth,
+  Articles,
+  Profile,
+  setToken: _token => {
+    token = _token;
+  }
+};
